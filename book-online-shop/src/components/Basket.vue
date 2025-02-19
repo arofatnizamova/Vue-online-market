98fc67a9ced667ff00a54cbb144f507085485acc
Arofat Nizamova <arofatnizamova@gmail.com>
2025-02-19 23:12:54 +0500

    created vue.app supported by ChatGPT

---
+++ book-online-shop/src/components/Basket.vue
@@ -0,0 +1,20 @@
+<template>
+    <table class="table text-center mt-5">
+      <tr v-for="(item, index) in items" :key="index">
+        <td>{{ index + 1 }}</td>
+        <td><img :src="item.img" style="width: 50px"></td>
+        <td>{{ item.name }}</td>
+        <td>{{ item.price }}</td>
+        <td><button @click="$emit('remove-item', index)" class="btn btn-danger">Remove</button></td>
+      </tr>
+    </table>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      items: Array
+    }
+  };
+  </script>
+  
---
+++ book-online-shop/src/components/ProductList.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="row mt-5 justify-content-evenly">
+      <div v-for="(item, index) in products" :key="index" class="col-lg-3">
+        <div class="w-100 h-100 shadow rounded-5 overflow-hidden">
+          <img :src="item.img" alt="" class="img-fluid object-fit-cover w-100">
+          <div class="px-3 py-3">
+            <span>{{ item.name }}</span>
+            <br>
+            <span>{{ item.price }}$</span>
+            <br>
+            <button @click="$emit('add-to-cart', item)" class="korzina btn-dark cursor-pointer btn">
+              добавить в корзину
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        products: [
+          { id: 1, img: '/1.jpg', name: 'Корейский язык', price: 100 },
+          { id: 2, img: '/2.jpg', name: 'Японский язык', price: 150 },
+          { id: 3, img: '/3.jpg', name: 'Немецкий язык', price: 350 },
+        ]
+      };
+    }
+  };
+  </script>
+  
