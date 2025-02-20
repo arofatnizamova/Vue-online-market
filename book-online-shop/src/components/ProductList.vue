7d75098fbaa312f29f0708915f957fcbc964f863
Arofat Nizamova <arofatnizamova@gmail.com>
2025-02-20 22:29:19 +0500

    chore: add product-list component

--- book-online-shop/src/components/ProductList.vue
+++ book-online-shop/src/components/ProductList.vue
@@ -1,33 +1,35 @@
 <template>
-    <div class="row mt-5 justify-content-evenly">
-      <div v-for="(item, index) in products" :key="index" class="col-lg-3">
-        <div class="w-100 h-100 shadow rounded-5 overflow-hidden">
-          <img :src="item.img" alt="" class="img-fluid object-fit-cover w-100">
-          <div class="px-3 py-3">
-            <span>{{ item.name }}</span>
-            <br>
-            <span>{{ item.price }}$</span>
-            <br>
-            <button @click="$emit('add-to-cart', item)" class="korzina btn-dark cursor-pointer btn">
-              добавить в корзину
-            </button>
-          </div>
+  <button @click="fetchData" class="btn btn-info mx-auto d-block my-5">НАЖМИТЕ, ЧТОБЫ УВИДЕТЬ ТОВАРЫ</button>
+  <div class="row mt-5 justify-content-evenly">
+    <div v-for="(item, index) in products" :key="index" class="col-lg-3">
+      <div class="w-100 h-100 shadow rounded-5 overflow-hidden">
+        <img :src="item.img" alt="" class="img-fluid object-fit-cover w-100">
+        <div class="px-3 py-3">
+          <span>{{ item.name }}</span>
+          <br>
+          <span>{{ item.price }}$</span>
+          <br>
+          <button class="korzina btn-dark cursor-pointer btn">
+            добавить в корзину
+          </button>
         </div>
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        products: [
-          { id: 1, img: '/1.jpg', name: 'Корейский язык', price: 100 },
-          { id: 2, img: '/2.jpg', name: 'Японский язык', price: 150 },
-          { id: 3, img: '/3.jpg', name: 'Немецкий язык', price: 350 },
-        ]
-      };
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      products: [],
+    };
+  },
+  methods: {
+    async fetchData() {
+        const response = await fetch("../../public/items.json");
+        this.products = await response.json();
     }
-  };
-  </script>
-  
+  }
+};
+</script>
--- book-online-shop/src/main.js
+++ book-online-shop/src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue';
-import App from './App.vue';
+import { createApp } from 'vue'
 
-createApp(App).mount('#app');
+import App from './App.vue'
+import ProductList from './components/ProductList.vue'
+
+const app = createApp(App)
+app.component('product-list', ProductList)
+app.mount('#app')
